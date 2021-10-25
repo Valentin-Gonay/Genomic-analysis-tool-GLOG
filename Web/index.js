5a0dfd543660bafc2f689c4695fb27f993224539
Valentin-Gonay <valentin.gonay@laposte.net>
2021-10-25 23:35:42 +0200

    nodejs takes index.html as home pages

--- Web/index.js
+++ Web/index.js
@@ -18,7 +18,7 @@ const app =  express ();
 app.use(express.json())
 
 
-app.get("/", (req, res) => res.sendFile(`${__dirname}/index_genomic.html`))
+app.get("/", (req, res) => res.sendFile(`${__dirname}/index.html`))
 app.use(express.static(__dirname))
 
 
