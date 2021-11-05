aaa37cdd8bab68cc1b9cf71290373ab46d4554ca
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-11-05 10:48:29 +0100

    test

---
+++ Python/make_bdblast.py
@@ -0,0 +1,4 @@
+import subprocess
+
+p1 = subprocess.run(['makeblastdb', '-in', 'DB', '-out', '/home/baudeau/bin/ncbi-blast/db.fasta',
+                     '-dbtype', 'nucl'], capture_output=True, text=True)
