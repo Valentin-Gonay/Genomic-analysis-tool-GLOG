8a9a99a1e4361787f353c157bbbbe0bab545ce30
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-11-06 11:05:44 +0100

    config file

--- Web/Python/init.py
+++ Web/Python/init.py
@@ -2,6 +2,8 @@ from os import error
 import platform
 import subprocess
 
+
+
 os=platform.system()
 print(os)
 typeerror=0
@@ -9,11 +11,12 @@ if os=="Linux":
     try:
         p1=subprocess.run(["blastn", "-version"] ,capture_output=True, text=True)
         res=(p1.stdout[0:5])
-        if(res):
+        if(res=='blast'):
             print('gg')
     except(error):
         print(error)
         typeerror=2
+        print(typeerror)
 elif "Windows" in os:
     try:
         p1=subprocess.run(["blastn", "-version"] ,capture_output=True, text=True,shell=True)
