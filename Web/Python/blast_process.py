2063fc50baf9d9502bbe371b41207a6ab710f987
Valentin-Gonay <valentin.gonay@laposte.net>
2021-11-06 15:32:55 +0100

    update blast

--- Web/Python/blast_process.py
+++ Web/Python/blast_process.py
@@ -1,9 +1,13 @@
 import subprocess
 
-def create_serv(PATH):
-    p1 = subprocess.run([PATH+'makeblastdb', '-in', '../Data/BD/test.fsa','-dbtype', 'nucl'], capture_output=True, text=True)
+def create_serv(PATH,OS):
+    PATH = PATH.strip('\n')
+    PATH = PATH.strip('\r')
+    p1 = subprocess.run([PATH+'makeblastdb', '-in', '../Data/BD/test.fsa','-dbtype', 'nucl'], capture_output=True, text=True, shell=bool(OS))
     print(p1.stdout)
 
-def blastn(PATH):
-    p1 = subprocess.run([PATH+'blastn','-db', '../Data/BD/test.fsa', '-query' ,'js/ref_ALPHA.fasta','-out','../Data/data/resu.txt'], capture_output=True, text=True)
+def blastn(PATH,OS):
+    PATH = PATH.strip('\n')
+    PATH = PATH.strip('\r')
+    p1 = subprocess.run([PATH+'blastn','-db', '../Data/BD/test.fsa', '-query' ,'js/ref_ALPHA.fasta','-out','../Data/data/resu.txt'], capture_output=True, text=True, shell=bool(OS))
     print(p1.stdout) 
