55e4cae7bad0c9e666aa2e14eb1f01ac1a3d104c
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-11-06 15:57:23 +0100

    bug correct for linux

--- Web/Python/blast_process.py
+++ Web/Python/blast_process.py
@@ -3,11 +3,13 @@ import subprocess
 def create_serv(PATH,OS):
     PATH = PATH.strip('\n')
     PATH = PATH.strip('\r')
-    p1 = subprocess.run([PATH+'makeblastdb', '-in', '../Data/BD/test.fsa','-dbtype', 'nucl'], capture_output=True, text=True, shell=bool(OS))
+    p1 = subprocess.run([PATH+'makeblastdb', '-in', '../Data/BD/test.fsa','-dbtype', 'nucl'], capture_output=True, text=True, shell=(OS=='True'))
     print(p1.stdout)
 
 def blastn(PATH,OS):
     PATH = PATH.strip('\n')
     PATH = PATH.strip('\r')
-    p1 = subprocess.run([PATH+'blastn','-db', '../Data/BD/test.fsa', '-query' ,'js/ref_ALPHA.fasta','-out','../Data/data/resu.txt'], capture_output=True, text=True, shell=bool(OS))
-    print(p1.stdout) 
+    p1 = subprocess.run(['blastn','-db', '../Data/BD/test.fsa', '-query' ,'js/ref_ALPHA.fasta','-out','../Data/data/resu.txt'], capture_output=True, text=True,shell=(OS=='True'))
+    print(p1.stdout)
+    print(p1.stderr)
+    
