a16f43bb858863c9c987e95263ed4467c7249824
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-11-06 15:57:46 +0100

    print en trop

--- Web/Python/blast_process.py
+++ Web/Python/blast_process.py
@@ -11,5 +11,5 @@ def blastn(PATH,OS):
     PATH = PATH.strip('\r')
     p1 = subprocess.run(['blastn','-db', '../Data/BD/test.fsa', '-query' ,'js/ref_ALPHA.fasta','-out','../Data/data/resu.txt'], capture_output=True, text=True,shell=(OS=='True'))
     print(p1.stdout)
-    print(p1.stderr)
+
     
