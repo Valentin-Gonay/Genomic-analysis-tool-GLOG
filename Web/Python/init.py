117f2dce1f2848e2355c6ce14dbac0ac6522c1ae
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-11-06 11:59:11 +0100

    ok

--- Web/Python/init.py
+++ Web/Python/init.py
@@ -2,28 +2,58 @@ from os import error
 import platform
 import subprocess
 
+def test_blastn(PATH=""):
 
+    os=platform.system()
+    type_error=0
+    if PATH!="":
+        PATH+'/'
+    if os=="Linux":
+        try:
+            p1=subprocess.run([PATH+"blastn", "-version"] ,capture_output=True, text=True)
+            res=(p1.stdout[0:5])
+            if(res != 'blast'):
+                type_error = 1
+        except(error):
+            type_error=2
 
-os=platform.system()
-print(os)
-typeerror=0
-if os=="Linux":
-    try:
-        p1=subprocess.run(["blastn", "-version"] ,capture_output=True, text=True)
-        res=(p1.stdout[0:5])
-        if(res=='blast'):
-            print('gg')
-    except(error):
-        print(error)
-        typeerror=2
-        print(typeerror)
-elif "Windows" in os:
-    try:
-        p1=subprocess.run(["blastn", "-version"] ,capture_output=True, text=True,shell=True)
-        res=(p1.stdout[0:5])
-        if(res):
-            print('gg')
+    elif "Windows" in os:
+        try:
+            p1=subprocess.run([PATH+"blastn", "-version"] ,capture_output=True, text=True,shell=True)
+            res=(p1.stdout[0:5])
+            if not (res):
+                type_error = 1
+        except(error):
+            type_error = 2
+    return type_error
 
-    except(error):
-        print(error)
-        typeerror=2
+def source_config():
+    config=open('config.txt','r')
+    for line in config:
+        if line[0:2]=="OS":
+            if line[3:len(line)-1]=="Linux":
+                OS=False
+                temp=config.readline()
+                PATH=temp[11:len(temp)]
+                if test_blastn(PATH)==0:
+                    return OS,PATH
+            else:
+                OS=False
+                temp=config.readline()
+                PATH=temp[11:len(temp)]
+                if test_blastn(PATH)==0:
+                    return OS,PATH
+    return ['Error','Error']
+
+def exec_init():
+    os=platform.system()
+    if test_blastn()==0:
+        if os=="Linux":
+            print(False,"")
+        else:
+            print(True,"")
+    else:
+        a,b=source_config()
+        print(a,b)
+
+exec_init()
