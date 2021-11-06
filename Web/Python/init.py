d6c750b18698ceb5ab2546761d072b1b6eae9c36
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-11-06 10:36:55 +0100

    init.py window compatible

--- Web/Python/init.py
+++ Web/Python/init.py
@@ -5,10 +5,19 @@ import subprocess
 os=platform.system()
 print(os)
 typeerror=0
-try:
-    p1=subprocess.run(["blastn", "-version"] ,capture_output=True, text=True)
-    print(p1.stdout[0:5])
+if os=="Linux":
+    try:
+        p1=subprocess.run(["blastn", "-version"] ,capture_output=True, text=True)
+        print(p1.stdout[0:5])
 
-except(error):
-    print(error)
-    typeerror=2
+    except(error):
+        print(error)
+        typeerror=2
+elif "Windows" in os:
+    try:
+        p1=subprocess.run(["blastn", "-version"] ,capture_output=True, text=True,shell=True)
+        print(p1.stdout[0:5])
+
+    except(error):
+        print(error)
+        typeerror=2
