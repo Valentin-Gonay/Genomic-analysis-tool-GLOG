9009d7c5975650ce6c71b0b3b091e75cb7048972
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-11-24 12:27:20 +0100

    ok

--- Web/js/class.js
+++ Web/js/class.js
@@ -1,1 +1,38 @@
 // CECI est le fichier de class
+
+
+class Sequence{
+    constructor(sequence, ID){
+        this.Sequence = sequence;
+        this.ID = ID;
+    }
+}
+
+class Project {
+    constructor(){
+        this.sequences=[];
+        this.resultat=[];
+        this.inputsequence='';
+    }
+    loadsequence(){
+        var txt=''
+    fetch('../Data/data/all_variant.fasta').then((function(response){
+        response.text().then((function(text){
+            let txt=text
+            console.log(txt)
+        }))
+    }))
+    }
+}
+
+class User{
+    constructor() {
+        this.Name = 'DefaultUser';
+        this.Project=[];
+    }
+}
+
+
+
+
+
---
+++ Web/js/main.js
@@ -0,0 +1,13 @@
+const defuser = new User();
+
+const main=new Main(defuser);
+
+// section import de fichiers
+
+
+// section graph
+
+
+//
+
+
