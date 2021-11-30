ce873263450ca9d8e708d6b2d6df8938eda66e66
loic-peron <peronloic97@gmail.com>
2021-11-30 11:11:32 +0100

    update_error_graph

--- Web/js/js_index.js
+++ Web/js/js_index.js
@@ -15,6 +15,8 @@ document.getElementById('menu_sequence').addEventListener('click',function() {
 });
 
 document.getElementById('menu_graph').addEventListener('click',function() {
+  createDropdownMenu();
+
   document.getElementById("input_window").style.display =  'none';
   document.getElementById("sequence_window").style.display =  'none';
   document.getElementById("graph_window").style.display =  'flex';
