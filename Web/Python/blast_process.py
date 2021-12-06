71ad009461b8a89a832b27dae39455c343bcc133
ThomasBaudeau <thomas.baudeau.33@gmail.com>
2021-12-06 15:56:56 +0100

    ok

--- Web/Python/blast_process.py
+++ Web/Python/blast_process.py
@@ -11,13 +11,12 @@ def blastn(PATH,OS):
     PATH = PATH.strip('\r')
     # DB CREMI : Data/BD/blast_2-9/
     # DB home : Data/BD/blast_2-12/
-    p1 = subprocess.run([PATH+'blastn','-db', 'Data/BD/blast_2-9/test.fsa', '-query' ,'Data/data/input.fasta','-out','Data/data/resu.txt'], capture_output=True, text=True,shell=(OS=='True'))
+    p1 = subprocess.run([PATH+'blastn','-db', 'Data/BD/blast_2-12/test.fsa', '-query' ,'Data/data/input.fasta','-out','Data/data/resu.txt'], capture_output=True, text=True,shell=(OS=='True'))
     print(p1.stdout)
 
 def writeinput(title,data):
     with open('Data/data/input.fasta','w') as f:
-        towrite=title+'\n'
-        f.write(towrite)
+        f.write(title)
         f.write(data)
         f.close()
         return
