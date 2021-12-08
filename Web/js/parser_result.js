629a0c49fb037165548c685892d9d8cc0b9b693b
Valentin-Gonay <valentin.gonay@laposte.net>
2021-12-08 14:54:12 +0100

    séparation de la fonction parser en plrs fonction regroupé dans 1 + modif css alignement seq

--- Web/js/parser_result.js
+++ Web/js/parser_result.js
@@ -3,70 +3,21 @@ function parser_res(text) {
   let tab = text.split('\n');
   let count = 0;
   let temp = '';
-  let query = {};
   let alignements = {};
+  let query
 
   let alignement_window = document.getElementById('sequence_window_text'); 
 
   for (let i = 0; i < tab.length; i++){
 
-    // Query
-    if (tab[i].startsWith('Query=') && temp === ''){
-      query.title = tab[i].slice(7,30);
-    }
+    query = query_parsing(tab[i])
 
-    if (tab[i].startsWith('Length=') && temp === ''){
-      query.length = tab[i].match(/\d+/)[0];
-    }
-    
-    // alignments
-    if (tab[i].startsWith('>')){
-      count ++;
-      temp = 'alignement_'+count;
-      alignements[temp] ={};
-      alignements[temp].title = tab[i].slice(1,24);
-      console.log("ULTIMATE TEST :\n",alignements[temp].title)
-      alignements[temp].seq = [];
-    }
-
-    if (tab[i].startsWith('Length=') && temp !== ''){
-      alignements[temp].length = tab[i].match(/\d+/)[0];  //Laisse de cote 
-      alignements[temp].score = tab[i+2].match(/\d+/)[0,0];  //Graph 1
-      alignements[temp].e_value = tab[i+2].match(/\d+\.\d+/)[0].replace('%','');  //Graph 1
-      alignements[temp].identities = tab[i+3].match(/\d+\/\d+/)[0,0];  //Graph 2(dans label)
-      alignements[temp].pIdentities = tab[i+3].match(/\d+\%/)[0,0].replace('%',''); //Graph 2
-      alignements[temp].gaps = tab[i+3].match(/(?<=Gaps = ).*(?=\s)/)[0];  //Graph 2(dans label)
-      alignements[temp].pGaps = tab[i+3].match(/\(([^()]*)\)$/)[0].replace('(','').replace('%)',''); // Graph 2      REVOIR one lign
-    }
-
-    if (tab[i].startsWith('Query') && temp !== ''){
-        //gestion des gaps et miss match
-        tab_1 = tab[i];
-        tab_2 = tab[i+2];
-
-        //nécessaire pour garder le texte aligné dans le html ni vue ni keunu
-        let pipe_line = tab[i+1].replaceAll(' ',".");
-        //pipe_line = pipe_line.replaceAll(/(?<=[.]{15})\.|(?<=[|])\.+/g,"<span id = 'sequence_dot'>*</span>");
-        pipe_line = pipe_line.replaceAll(/(?<=[.]{15})\.|(?<=\||\|\.*)\./g,"*");
-
-        //pipe_line = pipe_line.replaceAll('.',"<span id = 'sequence_dot'>.</span>");
-
-        let query_line = tab_1.replaceAll(' ',".");
-
-        let sequence_line = tab_2.replaceAll(' ',".");
-
-        let lines_formated = coloring(query_line,pipe_line,sequence_line);
-    
-        lines_formated[0] = lines_formated[0].replaceAll('.',"<span id = 'sequence_dot'>.</span>");
-        lines_formated[1] = lines_formated[1].replaceAll('.',"<span id = 'sequence_dot'>.</span>");
-        pipe_line = pipe_line.replaceAll('.',"<span id = 'sequence_dot'>.</span>");
-        pipe_line = pipe_line.replaceAll('*',"<span id = 'sequence_dot'>*</span>");
-
-        //creation alignment and pushing into alignments
-        alignements[temp].seq.push([lines_formated[0],pipe_line,lines_formated[1]+'<br>']);       
-    }
-
+    let res = alignments_parsing(tab[i],tab[i+1],tab[i+2],tab[i+3],count,alignements,temp)
+    temp = res[0];
+    alignements = res[1];
+    count = res[2];
   }
+
   const resu = new Resu(query, alignements);
   console.log(resu);
 
@@ -75,8 +26,6 @@ function parser_res(text) {
 
   return resu;
 }
-
-//parser_res(text);
 
 function coloring(query_line,pipe_line,sequence_line){
     var cure = 0;
@@ -96,7 +45,66 @@ function coloring(query_line,pipe_line,sequence_line){
     
         sequence_line = sequence_line.replace(regex,"<span id = 'sequence_miss_match'>"+sequence_line[position +pos]+"</span>");
     }
-
-
     return [query_line,sequence_line];
+}
+
+
+function query_parsing(line,temp){
+  let query = {};
+  if (line.startsWith('Query=') && temp === ''){
+    query.title = line.slice(7,30);
+  }
+
+  if (line.startsWith('Length=') && temp === ''){
+    query.length = line.match(/\d+/)[0];
+  }
+  return query
+}
+
+function alignments_parsing(line,line_1,line_2,line_3,count,alignements,temp){
+  if (line.startsWith('>')){
+    count ++;
+    temp = 'alignement_'+count;
+    alignements[temp] = {};
+    alignements[temp].title = line.slice(1,24);
+    alignements[temp].seq = [];
+  }
+
+  if (line.startsWith('Length=') && temp !== ''){
+    alignements[temp].length = line.match(/\d+/)[0];  //Laisse de cote 
+    alignements[temp].score = line_2.match(/\d+/)[0,0];  //Graph 1
+    alignements[temp].e_value = line_2.match(/\d+\.\d+/)[0].replace('%','');  //Graph 1
+    alignements[temp].identities = line_3.match(/\d+\/\d+/)[0,0];  //Graph 2(dans label)
+    alignements[temp].pIdentities = line_3.match(/\d+\%/)[0,0].replace('%',''); //Graph 2
+    alignements[temp].gaps = line_3.match(/(?<=Gaps = ).*(?=\s)/)[0];  //Graph 2(dans label)
+    alignements[temp].pGaps = line_3.match(/\(([^()]*)\)$/)[0].replace('(','').replace('%)',''); // Graph 2      REVOIR one lign
+  }
+
+  if (line.startsWith('Query') && temp !== ''){
+    //gestion des gaps et miss match
+    tab_1 = line;
+    tab_2 = line_2;
+
+    //nécessaire pour garder le texte aligné dans le html ni vue ni keunu
+    let pipe_line = line_1.replaceAll(' ',".");
+    //pipe_line = pipe_line.replaceAll(/(?<=[.]{15})\.|(?<=[|])\.+/g,"<span id = 'sequence_dot'>*</span>");
+    pipe_line = pipe_line.replaceAll(/(?<=[.]{15})\.|(?<=\||\|\.*)\./g,"*");
+
+    //pipe_line = pipe_line.replaceAll('.',"<span id = 'sequence_dot'>.</span>");
+
+    let query_line = tab_1.replaceAll(' ',".");
+
+    let sequence_line = tab_2.replaceAll(' ',".");
+
+    let lines_formated = coloring(query_line,pipe_line,sequence_line);
+
+    lines_formated[0] = lines_formated[0].replaceAll('.',"<span id = 'sequence_dot'>.</span>");
+    lines_formated[1] = lines_formated[1].replaceAll('.',"<span id = 'sequence_dot'>.</span>");
+    pipe_line = pipe_line.replaceAll('.',"<span id = 'sequence_dot'>.</span>");
+    pipe_line = pipe_line.replaceAll('*',"<span id = 'sequence_dot'>*</span>");
+
+    //creation alignment and pushing into alignments
+    alignements[temp].seq.push([lines_formated[0],pipe_line,lines_formated[1]+'<br>']);   
+  }
+  return [temp,alignements,count]
 }
